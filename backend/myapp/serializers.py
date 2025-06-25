e290660e5dd277c2d65792597d61af2b1d18eb8d
Muaazmaniyar <muaazmaniyar7@gmail.com>
2025-06-25 20:56:58 +0530

    dd

--- backend/myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-#Create Serializers FILE
-from rest_framework import serializers
-from .models import Bus
-
-class BusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bus
-        fields = '__all__'
